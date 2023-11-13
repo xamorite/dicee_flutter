3076b3daab3268ed5493bd582087dbab47c5f95b
Xamorite <eogunneye@gmail.com>
2023-11-13 13:01:56 +0100

    dice application for decision making between two individuals

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,15 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: Colors.black,
         appBar: AppBar(
-          title: Text('Dicee'),
-          backgroundColor: Colors.red,
+          title: Text('Settle Disputes'),
+          backgroundColor: Colors.black,
         ),
         body: DicePage(),
       ),
@@ -15,26 +17,45 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({super.key});
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftdice = 5;
+  int rightdice =2;
+
+  void changeNumber(){
+    setState(() {
+      leftdice = Random().nextInt(6)+1;
+      rightdice = Random().nextInt(6)+1;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Row(
         children: [
           Expanded(child:
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice1.png'),
-          ),
+          TextButton(onPressed: () {
+           changeNumber();
+          },
+              child: Image.asset('images/dice$leftdice.png')),
           ),
           Expanded(child:
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Image.asset('images/dice5.png'),
-          ),
+          TextButton(onPressed: (){
+            changeNumber();
+          },
+              child: Image.asset('images/dice$rightdice.png')),
           ),
         ],
       ),
+
     );
   }
 }
+
+
